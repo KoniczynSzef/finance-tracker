1fb2a018ab6aea67161c261e33e2d293af0988e0
KoniczynSzef <koniczynszef@gmail.com>
2024-11-30 12:24:57 +0100

    refactor: moved some fields from base to read

--- backend/src/schemas/user_schemas.py
+++ backend/src/schemas/user_schemas.py
@@ -9,14 +9,8 @@ class UserBase(BaseModel):
     email: str = Field(default="")
     full_name: str = Field(default="")
 
-    is_active: bool = Field(default=True)
-    is_verified: bool = Field(default=False)
-
     current_balance: Decimal = Field(default=Decimal(0.0))
     balance_threshold: Decimal = Field(default=Decimal(0.0))
-
-    created_at: datetime = Field(default=datetime.now())
-    updated_at: datetime = Field(default=datetime.now())
 
 
 class UserCreate(UserBase):
@@ -26,3 +20,9 @@ class UserCreate(UserBase):
 class UserRead(UserBase):
     id: int = Field()
     hashed_password: str = Field(default="")
+
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: datetime = Field(default=datetime.now())
+
+    is_active: bool = Field(default=True)
+    is_verified: bool = Field(default=False)
