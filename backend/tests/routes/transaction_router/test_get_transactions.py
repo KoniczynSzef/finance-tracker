1f3cd22bef2685cf46b3c4376afaf5165df8387e
KoniczynSzef <koniczynszef@gmail.com>
2024-12-07 11:29:43 +0100

    test: added tests for get transactions endpoint

--- backend/tests/routes/transaction_router/test_get_transactions.py
+++ backend/tests/routes/transaction_router/test_get_transactions.py
@@ -1,10 +1,12 @@
 
+from decimal import Decimal
 from os import getenv
 
 import pytest
 from database.config import get_session
 from schemas.auth_schemas import TokenData
 from services.auth_service import AuthService
+from src.models.transaction import Transaction
 from src.models.user import User
 from tests.api_setup import (
     client,
@@ -47,3 +49,68 @@ def test_get_transactions_endpoint_when_authenticated():
     response = client.get("/transactions", headers=auth_header)
 
     assert response.status_code == 200
+
+
+def test_get_transactions_endpoint_when_authenticated_and_no_transactions():
+    auth_service = AuthService(session)
+
+    token = auth_service.create_access_token(
+        TokenData(sub="Username Test", exp=60))
+    auth_header = {"Authorization": f"Bearer {token}"}
+
+    response = client.get("/transactions", headers=auth_header)
+    assert response.json() == []
+
+
+def test_get_transactions_endpoint_when_authenticated_and_transaction():
+    auth_service = AuthService(session)
+
+    transaction = Transaction(
+        id=1, name="Transaction 1", amount=Decimal(100), user_id=1)
+    session.add(transaction)
+    session.commit()
+    session.refresh(transaction)
+
+    token = auth_service.create_access_token(
+        TokenData(sub="Username Test", exp=60))
+
+    auth_header = {"Authorization": f"Bearer {token}"}
+    response = client.get("/transactions", headers=auth_header)
+
+    assert len(response.json()) == 1
+
+    assert response.json()[0]["id"] == 1
+    assert response.json()[0]["name"] == "Transaction 1"
+    assert Decimal(response.json()[0]["amount"]) == Decimal(100)
+
+
+def test_get_transactions_endpoint_when_authenticated_and_transactions():
+    auth_service = AuthService(session)
+
+    transaction = Transaction(
+        id=1, name="Transaction 1", amount=Decimal(100), user_id=1)
+
+    second_transaction = Transaction(
+        id=2, name="Transaction 2", amount=Decimal(200), user_id=1)
+
+    session.add(transaction)
+    session.add(second_transaction)
+    session.commit()
+    session.refresh(transaction)
+    session.refresh(second_transaction)
+
+    token = auth_service.create_access_token(
+        TokenData(sub="Username Test", exp=60))
+
+    auth_header = {"Authorization": f"Bearer {token}"}
+    response = client.get("/transactions", headers=auth_header)
+
+    assert len(response.json()) == 2
+
+    assert response.json()[0]["id"] == 1
+    assert response.json()[0]["name"] == "Transaction 1"
+    assert Decimal(response.json()[0]["amount"]) == Decimal(100)
+
+    assert response.json()[1]["id"] == 2
+    assert response.json()[1]["name"] == "Transaction 2"
+    assert Decimal(response.json()[1]["amount"]) == Decimal(200)
