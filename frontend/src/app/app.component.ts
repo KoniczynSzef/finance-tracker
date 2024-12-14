251e1c0dc4969031886c5e3086517ed0263ead83
KoniczynSzef <koniczynszef@gmail.com>
2024-12-14 18:08:12 +0100

    refactor: response type is just user

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { AuthService } from '../auth/auth.service';
+import { ResponseError } from '../types/auth/response-error.type';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.getCurrentUser().subscribe({
+      next: (user) => {
+        console.log(user);
+      },
+      error: (err: ResponseError) => {
+        console.error(err.error.detail);
+        // ! Unauthorized access
+      },
+    });
+  }
 }
--- frontend/src/auth/auth.service.ts
+++ frontend/src/auth/auth.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment.development';
-import { ResponseError } from '../types/auth/response-error.type';
 import { Token } from '../types/auth/token.type';
 import { User } from '../types/models/user.type';
 import { UserRead } from '../types/schemas/user-schemas.type';
@@ -18,7 +17,7 @@ export class AuthService {
   getCurrentUser() {
     const token = window.localStorage.getItem('jwt-token');
 
-    return this.httpClient.get<UserRead | ResponseError>(
+    return this.httpClient.get<UserRead>(
       `${this.API_URL}/auth/me?token=${token}`
     );
   }
