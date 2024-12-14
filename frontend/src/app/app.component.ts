c034c6c3893190ac3a3d65ba7a4efe9d1aaa41ec
KoniczynSzef <koniczynszef@gmail.com>
2024-12-14 16:35:46 +0100

    chore: created login form component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,55 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
-import { AuthService } from '../auth/auth.service';
-import { ResponseError } from '../types/auth/response-error.type';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'frontend';
-  constructor(private authService: AuthService) {}
-
-  ngOnInit(): void {
-    this.authService.getCurrentUser().subscribe({
-      next: (user) => {
-        if ('error' in user) {
-          return;
-        }
-
-        console.log(user);
-      },
-      error: (errorResponse: ResponseError) => {
-        console.log('Error Detail: ', errorResponse.error.detail);
-      },
-    });
-  }
-
-  handleClick() {
-    const username = window.prompt('Enter your username');
-    const password = window.prompt('Enter your password');
-
-    if (!username || !password) {
-      return;
-    }
-
-    this.authService.login(username, password).subscribe((res) => {
-      if ('access_token' in res) {
-        this.authService.saveTokenInLocalStorage(res);
-
-        this.authService.getCurrentUser().subscribe((res) => {
-          // TODO: logic to set authenticated user
-        });
-      }
-    });
-  }
-
-  handleLogout() {
-    this.authService.logout();
-    window.location.reload();
-  }
 }
