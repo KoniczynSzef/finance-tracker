eb65b2c6866bb10f3a37bc83eccfca5235cddce8
KoniczynSzef <koniczynszef@gmail.com>
2024-12-14 13:58:15 +0100

    chore: added simple logout functionality for better perspective of auth process

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -15,14 +15,13 @@ export class AppComponent implements OnInit {
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
-    this.authService.getUser().subscribe({
-      next: (res) => {
-        if ('error' in res) {
+    this.authService.getCurrentUser().subscribe({
+      next: (user) => {
+        if ('error' in user) {
           return;
         }
 
-        console.log(`res: ${res.username}`);
-        console.log(`res: ${res.email}`);
+        console.log(user);
       },
       error: (errorResponse: ResponseError) => {
         console.log('Error Detail: ', errorResponse.error.detail);
@@ -40,13 +39,17 @@ export class AppComponent implements OnInit {
 
     this.authService.login(username, password).subscribe((res) => {
       if ('access_token' in res) {
-        console.log(res);
         this.authService.saveTokenInLocalStorage(res);
 
-        this.authService.getUser().subscribe((res) => {
-          console.log(res);
+        this.authService.getCurrentUser().subscribe((res) => {
+          // TODO: logic to set authenticated user
         });
       }
     });
+  }
+
+  handleLogout() {
+    this.authService.logout();
+    window.location.reload();
   }
 }
--- frontend/src/auth/auth.service.ts
+++ frontend/src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment.development';
 import { ResponseError } from '../types/auth/response-error.type';
 import { Token } from '../types/auth/token.type';
+import { User } from '../types/models/user.type';
 import { UserRead } from '../types/schemas/user-schemas.type';
 
 @Injectable({
@@ -10,13 +11,11 @@ import { UserRead } from '../types/schemas/user-schemas.type';
 })
 export class AuthService {
   API_URL = environment.apiUrl;
+  user: User | null = null;
+
   constructor(private httpClient: HttpClient) {}
 
-  healthCheck() {
-    return this.httpClient.get<{ status: 'ok' }>(`${this.API_URL}/`);
-  }
-
-  getUser() {
+  getCurrentUser() {
     const token = window.localStorage.getItem('jwt-token');
 
     return this.httpClient.get<UserRead | ResponseError>(
@@ -41,5 +40,9 @@ export class AuthService {
 
   saveTokenInLocalStorage(token: Token) {
     window.localStorage.setItem('jwt-token', token.access_token);
+  }
+
+  logout() {
+    window.localStorage.removeItem('jwt-token');
   }
 }
