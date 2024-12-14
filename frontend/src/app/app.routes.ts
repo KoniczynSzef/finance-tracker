5f7692dc0fe3c1771d654f5a7f469f769588a3fb
KoniczynSzef <koniczynszef@gmail.com>
2024-12-14 17:25:20 +0100

    chore: started working on the login form using reactive forms

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginFormComponent } from './components/auth/login-form/login-form/login-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginFormComponent,
+  },
+];
--- frontend/src/app/components/auth/login-form/login-form/login-form.component.ts
+++ frontend/src/app/components/auth/login-form/login-form/login-form.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, InputTextModule, PasswordModule, ButtonModule],
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.scss'
+  styleUrl: './login-form.component.scss',
 })
 export class LoginFormComponent {
+  loginFormGroup = new FormGroup({
+    username: new FormControl<string>(''),
+    password: new FormControl<string>(''),
+  });
 
+  onSubmit() {
+    console.log(this.loginFormGroup.value);
+  }
 }
