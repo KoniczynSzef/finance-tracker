54db68159032d2a3f4d359f1f161f153abebf373
KoniczynSzef <koniczynszef@gmail.com>
2024-12-15 16:53:03 +0100

    refactor: added register component and sign up link button

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
-import { LoginFormComponent } from './components/auth/login-form/login-form/login-form.component';
+import { LoginFormComponent } from './components/auth/login-form/login-form.component';
+import { RegisterFormComponent } from './components/auth/register-form/register-form.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: LoginFormComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterFormComponent,
   },
   {
     path: 'dashboard',
--- frontend/src/app/components/auth/login-form/login-form.component.ts
+++ frontend/src/app/components/auth/login-form/login-form.component.ts
@@ -5,15 +5,15 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { Toast } from 'primeng/toast';
 import { catchError, of } from 'rxjs';
-import { AuthService } from '../../../../../auth/auth.service';
-import { ResponseError } from '../../../../../types/auth/response-error.type';
+import { AuthService } from '../../../../auth/auth.service';
+import { ResponseError } from '../../../../types/auth/response-error.type';
 
 @Component({
   selector: 'app-login-form',
@@ -24,6 +24,7 @@ import { ResponseError } from '../../../../../types/auth/response-error.type';
     PasswordModule,
     ButtonModule,
     Toast,
+    RouterModule,
   ],
   providers: [MessageService],
   templateUrl: './login-form.component.html',
