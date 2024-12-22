57f387ebfa720d7aa63e164fe37f36fad5e1c722
KoniczynSzef <koniczynszef@gmail.com>
2024-12-22 17:11:49 +0100

    chore: ilustrated logout behavior on dashboard

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,16 +1,38 @@
 import { Component, inject } from '@angular/core';
+import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { Toast } from 'primeng/toast';
+import { AuthService } from '../../../auth/auth.service';
 import { UserStateService } from '../../../store/user-state.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, Toast],
+  providers: [MessageService],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
   userStateService = inject(UserStateService);
+  authService = inject(AuthService);
+  messageService = inject(MessageService);
 
   user = this.userStateService.state$().user;
+
+  logout() {
+    this.authService.logout();
+    this.userStateService.unsetUser();
+
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Success',
+      detail: 'You have been successfully logged out.',
+      life: 3000,
+    });
+
+    setTimeout(() => {
+      this.userStateService.navigateToLogin();
+    }, 500);
+  }
 }
